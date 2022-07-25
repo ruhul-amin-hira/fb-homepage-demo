b525ad5db737003044e09ca00f04494ee53b09b6
Ruhul Amin Hira <ruhulaminhira63@gmail.com>
2022-07-26 00:59:48 +0600

    Creating layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
+import Contacts from './components/contacts/Contacts';
+import Header from './components/header/Header';
+import MainPage from './components/post/MainPage';
+import Profile from './components/profile/Profile';
 import './style.scss';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello from the app</h1>
+    <div className="relative">
+      <div className='fixed bg-blue-400'>
+        <Header />
+      </div>
+      <div>
+        <div>
+          <Profile />
+        </div>
+        <div>
+          <MainPage />
+        </div>
+        <div>
+          <Contacts />
+        </div>
+      </div>
     </div>
   );
 }
